d77ad45a47df759f1643334026aeae57d103d901
Allen Gao <agao323@gmail.com>
2017-02-17 18:36:55 -0800

    focus changes with input

--- js/index.js
+++ js/index.js
@@ -10,3 +10,45 @@ var limitInput = function(input) {
 		input.value = input.value.slice(0, input.maxLength);
 	}
 }
+
+var jump_to_2 = function(input) {
+	var len = input.value.length;
+	if (len > 0) {
+		document.getElementById("second").focus();
+	}
+	else {
+		document.getElementById("first").focus();
+	}
+}
+
+var jump_to_3 = function(input) {
+	var len = input.value.length;
+	if (len > 0) {
+		document.getElementById("third").focus();
+	}
+	else {
+		document.getElementById("second").focus();
+	}
+}
+
+var jump_to_4 = function(input) {
+	var len = input.value.length;
+	if (len > 0) {
+		document.getElementById("fourth").focus();
+	}
+	else {
+		document.getElementById("third").focus();
+	}
+}
+
+var jump_to_button = function(input) {
+	var len = input.value.length;
+	if (len > 0) {
+		document.getElementById("joinButton").focus();
+	}
+	else {
+		document.getElementById("fourth").focus();
+	}
+}
+
+
